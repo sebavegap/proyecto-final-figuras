a8989bcd4989e6b60400fac53642b54dee88a6d8
S Vega P <71044707+riffosaurus@users.noreply.github.com>
2023-03-30 20:01:03 -0300

    json inventario

---
+++ public/Inventario.js
@@ -0,0 +1,70 @@
+//array de objetos con inventario de figuras
+
+//las imagenes se deben cargar en la carpeta public y luego se debe importar la imagen en el componente que se va a mostrar
+//tambien podemos utilizar contexto para importar las imagenes
+
+ export const inventario = [
+    {
+        id: 1, 
+        nombre: "Dark Rey", 
+        linea: "Star Wars: The Black Series",
+        estado: "Como nueva, completa, con caja.",
+        cantidad: 1, 
+        precio: 25000,
+        imagen: "https://falabella.scene7.com/is/image/Falabella/gsc_110708127_253732_2?wid=800&hei=800&qlt=70",
+    },
+    {
+        id: 2, 
+        nombre: "Q9-0", 
+        linea: "Star Wars: The Black Series",
+        estado: "Como nuevo, completo, con caja.",
+        cantidad: 1, 
+        precio: 25000,
+        imagen: "https://www.questloot.cl/wp-content/uploads/2021/12/Q9-0-Zero-Star-Wars-Black-Series-2021-4.jpg",
+    },
+    {
+        id: 3, 
+        nombre: "Sharon Carter", 
+        linea: "Marvel Legends",
+        estado: "Nueva, sin BAF.",
+        cantidad: 1, 
+        precio: 15000,
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_727371-MLC51840722311_102022-O.jpg",
+    },
+    {
+        id: 4, 
+        nombre: "Doctor Octopus, Otto Octavius", 
+        linea: "Marvel Legends",
+        estado: "Como nuevo.",
+        cantidad: 0, 
+        precio: 40000,
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_820995-MLC31878775321_082019-O.jpg",
+    },
+    {
+        id: 5, 
+        nombre: "Black Cat", 
+        linea: "Marvel Legends",
+        estado: "Como nueva, sin gato.",
+        cantidad: 0, 
+        precio: 25000,
+        imagen: "https://topcam.cl/productos/n250001a260000/n255001a256000/n255101a255200/g255186.jpg",
+    },
+    {
+        id: 6, 
+        nombre: "He Who Remains", 
+        linea: "Marvel Legends",
+        estado: "Nuevo, sin BAF.",
+        cantidad: 1, 
+        precio: 15000,
+        imagen: "https://hasbrostore.cl/17670-large_default/figura-fan-marvel-legends-series-he-who-remains.jpg",
+    },
+    {
+        id: 7, 
+        nombre: "Classic Loki", 
+        linea: "Marvel Legends",
+        estado: "Nuevo, sin BAF.",
+        cantidad: 1, 
+        precio: 20000,
+        imagen: "https://cdnx.jumpseller.com/avso-toyz-store/image/27349826/resize/900/900?1663909497",
+    },
+];
