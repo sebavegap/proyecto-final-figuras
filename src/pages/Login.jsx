68deb4f7f70148db51ae1a908c66928fbcb272e5
S Vega P <71044707+riffosaurus@users.noreply.github.com>
2023-04-02 15:47:11 -0400

    forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
+//import de hooks de Bootstrap
+import { Form, Button } from 'react-bootstrap'
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+<h1>Ingresa a tu cuenta</h1>
+
+      <Form>
+      <Form.Group className="mb-3" controlId="formBasicUsername">
+        <Form.Label>Usuario</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+        </Form.Text>
+      </Form.Group>
+
+
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Contraseña</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Ingresar
+      </Button>
+    </Form>
+    </div>
   )
 }
 
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 
+//import de hooks de Bootstrap
+import { Form, Button } from 'react-bootstrap'
+
 const Registro = () => {
   return (
-    <div>Registro</div>
+    <div>
+<h1>Registra tu cuenta</h1>
+
+          <Form>
+      <Form.Group className="mb-3" controlId="formBasicUsername">
+        <Form.Label>Usuario</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          Este es el nombre que utilizarás para ingresar al sitio.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+      
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Contraseña</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Registrarse
+      </Button>
+    </Form>
+    </div>
   )
 }
 
