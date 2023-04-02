ee749aea3dd69a338909653019c6213eb57913b5
S Vega P <71044707+riffosaurus@users.noreply.github.com>
2023-04-02 15:30:21 -0400

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+//import de componentes de bootstrap
+import { Container, Row, Col, Button } from 'react-bootstrap'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>¡Bienvenido a El.Coleccionista!</h1>
+      <p>Aquí encontrarás una amplia selección de figuras de colección.</p>
+      <p>Para revisar el catalogo, ingresa con tu cuenta o regístrate.</p>
+
+      <Button>Ingresar</Button>
+      <Button>Registrarse</Button>
+    </div>
   )
 }
 
