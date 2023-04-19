881c9a28b12ed8c794f2a6501f32a4af115b1fd4
Eduardo Montenegro <edomontt.sepeda@gmail.com>
2023-04-19 18:01:53 -0300

    Actualización
    Commit 19/04/23

--- src/pages/Figura.jsx
+++ src/pages/Figura.jsx
@@ -1,5 +1,9 @@
 import React, { useContext, useState } from 'react'
 import { Button, Container } from 'react-bootstrap'
+
+//import de componentes para la navegación
+import { useNavigate } from 'react-router-dom'
+
 import Context from '../Context'
 
 const Figura = () => {
@@ -10,54 +14,76 @@ const Figura = () => {
     const [lineaFigura, setLineaFigura] = useState('');
     const [cantidad, setCantidad] = useState('');
 
-
     const { datosFiguras } = useContext(Context);
 
-    const registrarFigura = () => {
+    //creamos la variable navigate para usar más adelante
+    const navigate = useNavigate()
 
+    const registrarFigura = () =>
+    {
+        if (nombre && precio && imagen && lineaFigura && cantidad)
+        {
+            //manipulando el DOM
+            document.getElementById('nombreRegistro').disabled = true;
+            document.getElementById('precioRegistro').disabled = true;
+            document.getElementById('imagenRegistro').disabled = true;
+            document.getElementById('lineaUnoRegistro').disabled = true;
+            document.getElementById('lineaDosRegistro').disabled = true;
+            document.getElementById('cantidadRegistro').disabled = true;
         
+            const login = document.getElementById('registroFigura');
+        
+            login.innerText = 'Registrando...';
+            login.disabled = true;
 
-            const figura = {
+            //un segundo de espera
+            setTimeout(() =>
+            {
+                const figura =
+                {
+                    id: datosFiguras[0][datosFiguras[0].length - 1].id,
+                    nombre: nombre,
+                    linea: lineaFigura,
+                    estado: "Nuevo, con caja",
+                    cantidad: cantidad,
+                    precio: precio,
+                    imagen: imagen,
+                }
 
-                id: datosFiguras[0][datosFiguras[0].length - 1].id,
-                nombre: nombre,
-                linea: lineaFigura,
-                estado: "Nuevo, con caja",
-                cantidad: cantidad,
-                precio: precio,
-                imagen: imagen,
+                datosFiguras[0].push(figura);
 
-            }
-
-            datosFiguras[0].push(figura);
-
-       
-
+                //funcion para navegar al catalogo
+                navigate('/galeria');
+            }, 1000);
+        }
+        else
+        {
+          alert('Todos los campos son obligatorios');
+        }
     }
-
 
     return (
         <Container className='pageFigura bg-dark bg-opacity-50 w-25 p-5'>
             <h2>Agrega un producto</h2>
             <form>
 
-                <label className='mt-3' for="name">Nombre del producto</label><br />
-                <input type="text" id="name" onChange={(e) => setNombre(e.target.value)} autoComplete="off" /><br />
+                <label className='mt-3'>Nombre del producto</label><br />
+                <input type="text" id="nombreRegistro" onChange={(e) => setNombre(e.target.value)} autoComplete="off" /><br />
 
-                <label className='mt-3' for="price">Precio del producto</label><br />
-                <input type="number" id="price" onChange={(e) => setPrecio(e.target.value)} autoComplete="off" /><br />
+                <label className='mt-3'>Precio del producto</label><br />
+                <input type="number" id="precioRegistro" onChange={(e) => setPrecio(e.target.value)} autoComplete="off" /><br />
 
-                <label className='mt-3' for="price">¿Cuantos productos son?</label><br />
-                <input type="number" id="cant" onChange={(e) => setCantidad(e.target.value)} autoComplete="off" /><br />
+                <label className='mt-3'>¿Cuantos productos son?</label><br />
+                <input type="number" id="cantidadRegistro" onChange={(e) => setCantidad(e.target.value)} autoComplete="off" /><br />
 
-                <label className='mt-3' for="image">URL de la imagen</label><br />
-                <input type="text" id="url" onChange={(e) => setImagen(e.target.value)} autoComplete="off" /><br />
+                <label className='mt-3'>URL de la imagen</label><br />
+                <input type="text" id="imagenRegistro" onChange={(e) => setImagen(e.target.value)} autoComplete="off" /><br />
 
-                <label className='m-3' for="descript">Elige la linea de tu producto</label><br />
-                <input type="checkbox" id="cbox1" value="Star Wars: The Black Series" onChange={(e) => setLineaFigura(e.target.value)} /> Star Wars: The Black Series<br />
-                <input className='mb-4' type="checkbox" id="cbox2" value="Marvel Legends" onChange={(e) => setLineaFigura(e.target.value)} /> Marvel Legends<br />
+                <label className='m-3'>Elige la linea de tu producto</label><br />
+                <input type="checkbox" id="lineaUnoRegistro" value="Star Wars: The Black Series" onChange={(e) => setLineaFigura(e.target.value)} /> Star Wars: The Black Series<br />
+                <input className='mb-4' type="checkbox" id="lineaDosRegistro" value="Marvel Legends" onChange={(e) => setLineaFigura(e.target.value)} /> Marvel Legends<br />
 
-                <Button variant='primary' onClick={() => registrarFigura()}>Envia tu producto</Button>
+                <Button type="button" variant="primary" id="registroFigura" onClick={() => registrarFigura()}>Registrar</Button>
             </form>
         </Container>
     )
